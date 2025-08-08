da8860a5ccc8dc7e46295476e5981a44a03b043d
Suportes <suporte@vlcorrea.com.br>
2025-08-08 17:02:31 -0300

    feat: criação da página de produtos
    feat: criação da página de produtos

--- ReactIsisSeguros/src/App.jsx
+++ ReactIsisSeguros/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import Menu from './pags/Menu/Menu'
 import Banner from './pags/Banner/Banner'
 import QuemSomos from './pags/QuemSomos/QuemSomos'
-
+import Produtos from './pags/Produtos/Produtos'
 import Formulario from './pags/Formulario/Formulario'
 import Localizacao from './pags/Localizacao/Localizacao'
 import Footer from './pags/Footer/Footer'
@@ -18,7 +18,7 @@ function App() {
         <Menu />
         <Banner />
         <QuemSomos />
-
+        <Produtos />
         <Formulario />
         <Localizacao />
         <Footer />
